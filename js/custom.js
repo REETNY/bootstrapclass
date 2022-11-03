a3c74201b8268b2e47b5b97db297728623418bfe
REETNY <shamsideenajide@gmail.com>
2022-11-03 12:40:37 +0100

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,56 @@
-var tag = document.createElement('script');
-tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var modal = document.getElementById("videoModal");  // get the modal
+
+var openModal = document.getElementById("openModal"); // get the open modal button
+
+var closeBtn = document.getElementsByClassName("closeBtn")[0];  // get the close modal button
+
+// listen for open click
+openModal.addEventListener('click', Modalopen);
+
+//function fo Modalopen
+function Modalopen(){
+    modal.style.display = 'flex'
+}
+
+// listen for close click
+closeBtn.addEventListener('click', Modalclose);
+
+//function fo Modalopen
+function Modalclose(){
+    modal.style.display = 'none'
+}
+
+
+
+//youtube
+
+// https://developers.google.com/youtube/iframe_api_reference
+
+// global variable for the player
 var player;
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('music', {
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player("video", {
     events: {
-      'onReady': onPlayerReady
+      // call this function when player is ready to use
+      onReady: onPlayerReady
     }
   });
 }
 
 function onPlayerReady(event) {
-  document.getElementById('pasVid').addEventListener('click', function() {
-    player.pauseVideo();
-  })
+  // bind events
+
+  var pauseButton = document.getElementById("closeYut");
+  pauseButton.addEventListener("click", function () {
+    player.stopVideo();
+  });
 }
+
+// Inject YouTube API script
+var tag = document.createElement("script");
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
